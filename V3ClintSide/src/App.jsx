7e4bf5c26b449c07e2cafce766e37d6817a27ec7
sri <sridhark3773@gmail.com>
2025-07-29 17:13:02 +0530

    Update App.jsx by mobile

--- V3ClintSide/src/App.jsx
+++ V3ClintSide/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}/>
+        <Route path="/" element={<LoginPage />}/>
        
       </Routes>
       </BrowserRouter>
