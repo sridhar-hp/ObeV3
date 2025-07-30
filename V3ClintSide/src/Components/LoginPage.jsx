a5b659e52e248fb36b1b038c65c602cd0b808dae
sri <sridhark3773@gmail.com>
2025-07-30 08:48:16 +0530

    Update LoginPage.jsx

--- V3ClintSide/src/Components/LoginPage.jsx
+++ V3ClintSide/src/Components/LoginPage.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 function LoginPage()
 {
+    const [staffId,setStaffId]=useState([])
+    
     return(
 
         <>
-        <h1>hello</h1></>
+        <h1>hello</h1>
+        <div> 
+        
+        <input placeholder="staffId"
+            vlue={}
+            onChange={setStaffId(e=></input>)> </input>
+        <input placeholder="staffPass"></input>
+ 		<button> LOGIN </button>
+        </div>
+        
+        </>
     )
 }
 export default LoginPage;
