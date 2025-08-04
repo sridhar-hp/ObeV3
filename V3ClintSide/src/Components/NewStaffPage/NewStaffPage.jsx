0e051cb51dc4916fe33ba2e8e9476fefd758ab51
sri <sridhark3773@gmail.com>
2025-08-04 10:58:25 +0530

    Update NewStaffPage.jsx

--- V3ClintSide/src/Components/NewStaffPage/NewStaffPage.jsx
+++ V3ClintSide/src/Components/NewStaffPage/NewStaffPage.jsx
@@ -1,12 +1,60 @@
 import React from "react";
+import {useState} from "reactrouterdom";
 
 function NewStaffPage() {
+  const [newStaff,setNewStaff]=useState();
+
+  
   return (
     <div>
       <h1>New Staff Page</h1>
+     
       <p>This is where you can add new staff members.</p>
+   <div> 
+      <div> 
+      <input placeholder="staffname"
+        name="staffName"> </input>
+      </div>
+      <div> 
+        
+      <input placeholder="staffid"
+        name="staffId"> </input>
+     </div>
+        <div> 
+        <input placeholder="class"
+          name="class"> </input>
+     </div>
+        <div> 
+        <input placeholder="debartment"
+          name="dbt"> </input>
+     </div>
+        <div> 
       
+          <input placeholder="semyear"
+            name="sem"> </input>
     </div>
+         </div>
+        </div>
+     
   );
 }
 export default NewStaffPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
