6c3c5e0484e13038b6b337784afa75fac3fb5aa4
sridhar-hp <sridhark3773@gmail.com>
2025-08-06 20:18:56 +0530

    deleting both course and staff login

--- V3ClintSide/src/Components/EditStaffPage/EditStaffPage.jsx
+++ V3ClintSide/src/Components/EditStaffPage/EditStaffPage.jsx
@@ -43,17 +43,31 @@ function EditStaffPage() {
     return (
 
         <>
-            <div className="editdetitbox">
-                <ul className="eul">{staffDetils.map((detiles, index) => (
-                    <li className="eli" key={index}>
-                        <h1 className="eh1 ">name: {detiles.staff_name}</h1>
-                        <h3 className="eh3">staffid:{detiles.staff_id}</h3>
-                        <h5 className="eh5">course_title:{detiles.cours_title}</h5>
-                        <p className="ep">sem :{detiles.academic_sem}</p>
-                        <button className="ed" onClick={()=>{handleDelet(detiles.staff_id)}}>delete</button>
-                    </li>
+            <div className="editdetitpopupbox">
+                <table >
+                    <thead >
+                    <tr className="esheding">
+                        <th>s.no</th>
+                        <th>NAME</th>
+                        <th>STAFF ID</th>
+                        <th>courses</th>
+                        <th>semsaster</th>
+                        <th>EDIT</th>
+                    </tr>
+                    </thead>
+
+                <tbody>{staffDetils.map((detiles, index) => (
+                      <tr className="esbodycontent" key={index}>
+                        <td>{index+1}</td>
+                        <td>{detiles.staff_name}</td>
+                        <td>{detiles.staff_id}</td>
+                        <td>{detiles.cours_title}</td>
+                        <td>{detiles.academic_sem}</td>
+                        <td><button  onClick={()=>{handleDelet(detiles.staff_id)}}>delete</button></td>
+                    </tr> 
                 ))}
-                </ul>
+                </tbody>
+                </table>
             </div>
         </>
 
@@ -61,3 +75,13 @@ function EditStaffPage() {
 }
 
 export default EditStaffPage;
+
+// {staffDetils.map((detiles, index) => (
+//                     <li className="eli" key={index}>
+//                         <h1 className="eh1 ">name: {detiles.staff_name}</h1>
+//                         <h3 className="eh3">staffid:{detiles.staff_id}</h3>
+//                         <h5 className="eh5">course_title:{detiles.cours_title}</h5>
+//                         <p className="ep">sem :{detiles.academic_sem}</p>
+//                         <button className="ed" onClick={()=>{handleDelet(detiles.staff_id)}}>delete</button>
+//                     </li>
+//                 ))}
--- V3ClintSide/src/Components/NewStaffPage/NewStaffPage.jsx
+++ V3ClintSide/src/Components/NewStaffPage/NewStaffPage.jsx
@@ -1,47 +1,70 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import "./NewStaffPage.css";
+import axios from 'axios';
 
 function NewStaffPage() {
-  const [newStaff,setNewStaff]=useState({
-   staff
-    
-  });
-
-  const handleNewStaff=async()=>{
+    const [newstaff, setNewStaff] = useState({});
 
 
-  }
-  
-  return (
-    <>
-    <div className="nsbg">
-   <div className="nspopup"> 
+    const handleNewStaff = async(event) => {
+        event.preventDefault();// e.preventDefault();
+        try {
+            console.log(newstaff);
 
-      <div className="ns" > 
-      <input className="ns1" placeholder="staffname" name="staffName"/> 
-      </div>
+            const res = await axios.post("http://localhost:5000/Admin/newstaff", newstaff)
+            if(res.data.success)
+            {
+            alert("add success");
+            }
+            else{
+                alert("canot add ");
+            }
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
 
-      <div className="ns"> 
-      <input className="ns1" placeholder="staffid" name="staffId"/>
-      </div>
+    const handleInput = (event) => {
+        const { name, value } = event.target;
+        setNewStaff((prev) => ({ ...prev, [name]: value }));
+    };
 
-        <div className="ns"> 
-        <input className="ns1" placeholder="class"name="class"/> 
-        </div>
+    return (
+        <>
+            <form onSubmit={handleNewStaff} className="nsbg">
+                <div className="nspopup">
 
-        <div className="ns"> 
-        <input className="ns1" placeholder="debartment" name="dbt"/>
-        </div>
+                    <div className="ns" >
+                        <input className="ns1" placeholder="staffname" name="staff_name" onChange={handleInput} required />
+                    </div>
 
-          <div className="ns"> 
-          <input className="ns1" placeholder="password" name="pass"/>
-         </div>
-          <button className="nssubmit" type="submit"> submit </button>
-         </div>
-        </div>
-     </>
-  );
+                    <div className="ns">
+                        <input className="ns1" placeholder="staffid" name="staff_id" onChange={handleInput} required />
+                    </div>
+
+                    <div className="ns">
+                        <input className="ns1" placeholder="course title" name="course_title" onChange={handleInput} required />
+                    </div>
+
+                    <div className="ns">
+                        <input className="ns1" placeholder="department" name="dept_name" onChange={handleInput} required />
+                    </div>
+
+                     <div className="ns">
+                        <input className="ns1" placeholder="academic sem" name="academic_sem" onChange={handleInput} required />
+                    </div>
+
+
+                    <div className="ns">
+                        <input className="ns1" placeholder="password" name="staff_pass" onChange={handleInput} required />
+                    </div>
+                    <button className="nssubmit" type="submit"> submit </button>
+                </div>
+            </form>
+        </>
+    );
 }
 export default NewStaffPage;
 
