c8f5c8d7c9370ab4d9e29ba5e9e115a8be5da212
sridhar-hp <sridhark3773@gmail.com>
2025-09-03 07:16:10 +0530

    mark entry page set done

--- V3ClintSide/src/Components/MarkEntry/MarkEntry.jsx
+++ V3ClintSide/src/Components/MarkEntry/MarkEntry.jsx
@@ -6,34 +6,37 @@ function MarkEntry() {
     return (
         <>
             <h1 className="dummy">this is mark entry page</h1>
-            <Link to="/LayoutPage/jmc001/Classes">back</Link>
+            <div className="meback">
+                <Link to="/LayoutPage/jmc001/Classes">back</Link>
+                </div>
             <div className="mecontiner">
-            <table>
-                <thead >
-                    <tr className="tme">
-                        <th>register no</th>
-                        <th>class</th>
-                        <th>section</th>
-                        <th>LOT</th>
-                        <th>MOT</th>
-                        <th>HOT</th>
-                        <th>TOTAL</th>
-                    </tr>
-                </thead>
+                <table className="mebox">
+                    <thead >
+                        <tr className="tme">
+                            <th>register no</th>
+                            <th>class</th>
+                            <th>section</th>
+                            <th>LOT</th>
+                            <th>MOT</th>
+                            <th>HOT</th>
+                            <th>TOTAL</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    <tr className="bme">
-                        <td>123123</td>
-                        <td>bca</td>
-                        <td>e</td>
-                        <td>lot mark</td>
-                        <td>mot mark</td>
-                        <td>hot mark</td>
-                        <td>total</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button className="me">submit</button>
+                    <tbody>
+                        <tr className="bme">
+                            <td>123123</td>
+                            <td>bca</td>
+                            <td>e</td>
+                            <td>lot mark</td>
+                            <td>mot mark</td>
+                            <td>hot mark</td>
+                            <td>total</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button className="me">submit</button>
+
             </div>
         </>
     );
